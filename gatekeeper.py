d409a732dce74d5717565cae0cf5694e928d25d0
OldBigBuddha <develoment@oldbigbuddha.net>
2019-05-21 20:12:24 +0300

    [update] on_reacrion_add -> on_raw_reaction_add
    Update some enviroment value name.

--- gatekeeper.py
+++ gatekeeper.py
@@ -30,57 +30,31 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 # 新しく入ってきた人のみが見れるチャンネル、ここのメッセージにリアクションをすると @member が付与される。
-WELCOME_CHANNEL_ID = int( os.getenv('WELCOME_ID') )
+WELCOME_CHANNEL_ID = int( os.getenv('WELCOME_CHANNEL_ID') )
 
 # @member が付与されたことを通知するチャンネル
-MEMBER_CHANNEL_ID  = int( os.getenv('MEMBER_ID') )
+ENTRY_NOTIFY_CHANNEL_ID = int(os.getenv('ENTRY_NOTIFY_CHANNEL_ID'))
 
-# 起動時に WELCOME_CHANNEL へ投稿するメッセージ
-# なんか Bot 起動前のメッセージには on_reaction_add が反応しないっぽい？
-welcome_message = '''**「OJI の部屋」へようこそ**
-この鯖は主に OJI が実験場として使う鯖です。
-OJI が作ってみたソフトウェアがいち早くさわれたり、開発段階から関係を持つこともできちゃいます。
-また、日々の情報収集の中でこれはぜひみんなに知ってほしいなと思った情報等も共有していきます。
-ほかにも日頃の愚痴であったり失敗談であったりと Twitter などでは見れない意外な一面も見れちゃいます。
+# @member の ID
+MEMBER_ROLE_ID = int(os.getenv('MEMBER_ROLE_ID'))
 
-OJI という生体に興味がある方は、以下の注意事項に目を通して最後に **Agreement** の意味を込めてこのメッセージに :white_check_mark: をしてください。
-本鯖の門衛 Bot が、あなたに本鯖で楽しむために必要な役職を与えてくれるでしょう。
-
-**注意事項**
-- 「OJI の部屋」(以下、「本鯖」。)内で起こったいかなる事象においても OJI 及び本鯖のメンバーは **一切責任を持ちません** 。
-- **本鯖内における絶対ルールは OJI の意志と気分です。**
-- いろいろな方がいるので、想像力をはたらかせながらコミュニケーションを取りましょう。
-- **Don't be a dick, be smart.**
-'''
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('ログインしたにゃ')
-    welcome_channel = client.get_channel(WELCOME_CHANNEL_ID)
-    await welcome_channel.purge()
-
-    # Welcome Message の投稿
-    await welcome_channel.send(welcome_message)
-
-    # 新規ユーザーが Agreement の意味を込めて追加するリアクションを先に設定しておく
-    await welcome_channel.last_message.add_reaction('✅')
 
 @client.event
-async def on_reaction_add(reaction, user):
-    # 鯖内でリアクションがされた際の処理
-    if user.bot or reaction.message.channel.id != WELCOME_CHANNEL_ID:
-        # Bot からリアクションと Welcome Channel 以外のリアクションを無視
-        return
+async def on_raw_reaction_add(payload):
+    received_channel_id = client.get_channel(payload.channel_id).id
 
-    # @member を取得
-    role = discord.utils.get(reaction.message.guild.roles, name='member')
+    if received_channel_id == WELCOME_CHANNEL_ID:
+        guild = client.get_guild(payload.guild_id)
+        user = guild.get_member(payload.user_id)
+        member_role = guild.get_role(MEMBER_ROLE_ID)
 
-    # @member をリアクションをしたユーザへ付与
-    await user.add_roles(role)
-
-    # @member を付与した旨を Member Channel へ流す
-    await client.get_channel(MEMBER_CHANNEL_ID).send(f'{user} に {role.name} を付与したにゃ。')
+        await user.add_roles(member_role)
+        await guild.get_channel(ENTRY_NOTIFY_CHANNEL_ID).send(f'{user.name} が member を取得したにゃ!')
 
 client.run(TOKEN)
